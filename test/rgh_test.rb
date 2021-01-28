0a675d533f3dfdb003e055e2569b43cb307caf5f
shiiotaka <shiiotaka@gmail.com>
2021-01-28 16:26:30 +0900

    4.8

--- test/rgh_test.rb
+++ test/rgh_test.rb
@@ -14,3 +14,13 @@ class RghTest < Minitest::Test
     assert_equal [4, 60, 120], to_ints('#043c78')
   end
 end
+
+
+File.open("./sample.txt", "w") do |file|
+  file.puts("1行目のテキストです")
+  file.puts("2行目のテキストです")
+  file.puts("3行目のテキストです")
+  file.puts("3行目のテキストです")
+  file.puts("3行目のテキストです")
+  file.puts("3行目のテキストです")
+end
